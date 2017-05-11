55b8a38dc9e2b8e4ddaa87796aef3b29f6f0ec2f
Fr-d-rik <frederikharder@gmail.com>
2017-05-11 16:58:03 +0200

    prepping 10 layer vgg inversion run

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 # deep_restoration - MSc AI Thesis
 
 ### Next Steps:
-- change architecture: each module specifies in and out tensor (optionally either from last module or classifier) and whether reconstruction becomes part of the loss
-- create reconstructions for deeper vgg layers and alexnet
 - find a way for loading dosovitskiy's caffe model into tensorflow or extract weights somehow
 - add split loss option per module
 - conitue training of later vgg layers to actual convergence
-- enable loading weights to continue training (done)
-- create resized dataset for runtime speedups (done)
 - adapt code to allow stacking models (done - for now)
 - pretty up plotting functions (done - for now)
-- fix visualization for net images
-
+- consider adding parts of curet texture data set to selected images
+- work through and reimplement mahendran & vedaldi's paper
 
 ### Notes
 - learning rate at 0.0003 seems good. 0.001 becomes a bit unstable (as in vgg16/l1_cd/run4)
@@ -19,6 +15,3 @@
 - chose bmp over png for 3x faster acces at cost of 150% space
 
 #### Run Schedule
-
-curet texture
-vedaldi 19
--- server_script.py
+++ server_script.py
@@ -1,134 +1,83 @@
 from net_inversion import NetInversion
 from parameter_utils import default_params
 
+
 spec1 = dict(inv_model_type='deconv_conv',
-             op1_height=3, op1_width=3, op1_strides=[1, 2, 2, 1], op1_pad='VALID',
+             op1_height=5, op1_width=5, op1_strides=[1, 2, 2, 1], op1_pad='SAME',
              op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
-             hidden_channels=96, target_shape=[55, 55, 96],
-             inv_input_name='pool1:0', inv_target_name='conv1/relu:0',
-             rec_name='conv1_rec', add_loss=True)
+             hidden_channels=256, target_shape=[28, 28, 256],
+             inv_input_name='pool3:0', inv_target_name='conv3_3/relu:0',
+             rec_name='conv3_3_rec', add_loss=True)
 
 spec2 = dict(inv_model_type='deconv_conv',
-             op1_height=11, op1_width=11, op1_strides=[1, 4, 4, 1], op1_pad='SAME',
-             op2_height=11, op2_width=11, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
-             hidden_channels=96, target_shape=[224, 224, 96],
-             inv_input_name='module_1/conv1_rec:0', inv_target_name='bgr_normed:0',
-             rec_name='reconstruction', add_loss=True)
+             op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1], op1_pad='SAME',
+             op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
+             hidden_channels=256, target_shape=[56, 56, 256],
+             inv_input_name='conv3_3_rec:0', inv_target_name='conv3_2/relu:0',
+             rec_name='conv3_2_rec', add_loss=True)
 
-params = dict(classifier='alexnet',
-              inv_model_specs=[spec1, spec2],
-              log_path='./logs/net_inversion/alexnet/l12_2dc/run3/',
-              load_path='./logs/net_inversion/alexnet/l12_2dc/run2/ckpt-3000')
+spec3 = dict(inv_model_type='deconv_conv',
+             op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1], op1_pad='SAME',
+             op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
+             hidden_channels=256, target_shape=[56, 56, 256],
+             inv_input_name='conv3_2_rec:0', inv_target_name='conv3_1/relu:0',
+             rec_name='conv3_1_rec', add_loss=True)
+
+spec4 = dict(inv_model_type='deconv_conv',
+             op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1], op1_pad='SAME',
+             op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
+             hidden_channels=256, target_shape=[56, 56, 128],
+             inv_input_name='conv3_1_rec:0', inv_target_name='pool2:0',
+             rec_name='pool2_rec', add_loss=True)
+
+spec5 = dict(inv_model_type='deconv_conv',
+             op1_height=5, op1_width=5, op1_strides=[1, 2, 2, 1], op1_pad='SAME',
+             op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
+             hidden_channels=128, target_shape=[112, 112, 128],
+             inv_input_name='pool2_rec:0', inv_target_name='conv2_2/relu:0',
+             rec_name='conv2_2_rec', add_loss=True)
+
+spec6 = dict(inv_model_type='deconv_conv',
+             op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1], op1_pad='SAME',
+             op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
+             hidden_channels=128, target_shape=[112, 112, 128],
+             inv_input_name='conv2_2_rec:0', inv_target_name='conv2_1/relu:0',
+             rec_name='conv2_1_rec', add_loss=True)
+
+spec7 = dict(inv_model_type='deconv_conv',
+             op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1], op1_pad='SAME',
+             op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
+             hidden_channels=128, target_shape=[112, 112, 64],
+             inv_input_name='conv2_1_rec:0', inv_target_name='pool1:0',
+             rec_name='pool1_rec', add_loss=True)
+
+spec8 = dict(inv_model_type='deconv_conv',
+             op1_height=5, op1_width=5, op1_strides=[1, 2, 2, 1], op1_pad='SAME',
+             op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
+             hidden_channels=64, target_shape=[224, 224, 64],
+             inv_input_name='pool1_rec:0', inv_target_name='conv1_2/relu:0',
+             rec_name='conv1_2_rec', add_loss=True)
+
+spec9 = dict(inv_model_type='deconv_conv',
+             op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1], op1_pad='SAME',
+             op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
+             hidden_channels=64, target_shape=[224, 224, 64],
+             inv_input_name='conv1_2_rec:0', inv_target_name='conv1_1:0',
+             rec_name='conv1_1_rec', add_loss=True)
+
+spec10 = dict(inv_model_type='deconv_conv',
+              op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1], op1_pad='SAME',
+              op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1], op2_pad='SAME',
+              hidden_channels=64, target_shape=[224, 224, 3],
+              inv_input_name='conv1_1_rec:0', inv_target_name='bgr_normed:0',
+              rec_name='reconstruction', add_loss=True)
+
+params = dict(classifier='vgg16',
+              inv_model_specs=[spec1, spec2, spec3, spec4, spec5, spec6, spec7, spec8, spec9, spec10],
+              log_path='./logs/net_inversion/vgg16/l1-10_10dc/run1/',
+              load_path='')
 params.update(default_params())
 print(params)
 ni = NetInversion(params)
 ni.train()
 
-
-# specs = dict(op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1],
-#              op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1],
-#              hidden_channels=128, target_shape=[112, 112, 128])
-#
-# params = dict(classifier='vgg16', inv_input_name='conv2_1/relu:0', inv_target_name='pool1:0',
-#               inv_model_type='deconv_conv',
-#               inv_model_specs=specs,
-#               log_path='./logs/layer_inversion/vgg16/l4_dc/run1/',
-#               load_path='')
-# params.update(default_params())
-# print(params)
-# li = LayerInversion(params)
-# li.train()
-#
-#
-# specs = dict(op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1],
-#              op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1],
-#              hidden_channels=128, target_shape=[112, 112, 128])
-#
-# params = dict(classifier='vgg16', inv_input_name='conv2_2/relu:0', inv_target_name='conv2_1/relu:0',
-#               inv_model_type='deconv_conv',
-#               inv_model_specs=specs,
-#               log_path='./logs/layer_inversion/vgg16/l5_dc/run1/',
-#               load_path='')
-# params.update(default_params())
-# print(params)
-# li = LayerInversion(params)
-# li.train()
-#
-#
-# specs = dict(op1_height=5, op1_width=5, op1_strides=[1, 2, 2, 1],
-#              op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1],
-#              hidden_channels=128, target_shape=[112, 112, 128])
-#
-# params = dict(classifier='vgg16', inv_input_name='pool2:0', inv_target_name='conv2_2/relu:0',
-#               inv_model_type='deconv_conv',
-#               inv_model_specs=specs,
-#               log_path='./logs/layer_inversion/vgg16/l6_dc/run1/',
-#               load_path='')
-# params.update(default_params())
-# print(params)
-# li = LayerInversion(params)
-# li.train()
-#
-#
-# specs = dict(op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1],
-#              op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1],
-#              hidden_channels=256, target_shape=[56, 56, 128])
-#
-# params = dict(classifier='vgg16', inv_input_name='conv3_1/relu:0', inv_target_name='pool2:0',
-#               inv_model_type='deconv_conv',
-#               inv_model_specs=specs,
-#               log_path='./logs/layer_inversion/vgg16/l7_dc/run1/',
-#               load_path='')
-# params.update(default_params())
-# params['num_iterations'] = 5000
-# print(params)
-# li = LayerInversion(params)
-# li.train()
-#
-#
-# specs = dict(op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1],
-#              op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1],
-#              hidden_channels=256, target_shape=[56, 56, 256])
-#
-# params = dict(classifier='vgg16', inv_input_name='conv3_2/relu:0', inv_target_name='conv3_1/relu:0',
-#               inv_model_type='deconv_conv',
-#               inv_model_specs=specs,
-#               log_path='./logs/layer_inversion/vgg16/l8_dc/run1/',
-#               load_path='')
-# params.update(default_params())
-# params['num_iterations'] = 5000
-# print(params)
-# li = LayerInversion(params)
-# li.train()
-#
-#
-# specs = dict(op1_height=5, op1_width=5, op1_strides=[1, 1, 1, 1],
-#              op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1],
-#              hidden_channels=256, target_shape=[56, 56, 256])
-#
-# params = dict(classifier='vgg16', inv_input_name='conv3_3/relu:0', inv_target_name='conv3_2/relu:0',
-#               inv_model_type='deconv_conv',
-#               inv_model_specs=specs,
-#               log_path='./logs/layer_inversion/vgg16/l9_dc/run1/',
-#               load_path='')
-# params.update(default_params())
-# params['num_iterations'] = 5000
-# print(params)
-# li = LayerInversion(params)
-# li.train()
-#
-#
-# specs = dict(op1_height=5, op1_width=5, op1_strides=[1, 2, 2, 1],
-#              op2_height=5, op2_width=5, op2_strides=[1, 1, 1, 1],
-#              hidden_channels=256, target_shape=[28, 28, 256])
-#
-# params = dict(classifier='vgg16', inv_input_name='pool3:0', inv_target_name='conv3_3/relu:0',
-#               inv_model_type='deconv_conv',
-#               inv_model_specs=specs,
-#               log_path='./logs/layer_inversion/vgg16/l10_dc/run1/',
-#               load_path='')
-# params.update(default_params())
-# params['num_iterations'] = 5000
-# print(params)
-# li = LayerInversion(params)
-# li.train()
