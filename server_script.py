3ef79cfb3f63516a24886630c5fd490bd6022637
Fr-d-rik <frederikharder@gmail.com>
2017-05-16 21:48:54 +0200

    prepping mahendran vedaldi run for remaining vgg layers

--- server_script.py
+++ server_script.py
@@ -9,7 +9,7 @@ params = dict(image_path='./data/selected/images_resized/val13_monkey.bmp', laye
               learning_rate=0.004,
               num_iterations=2000,
               print_freq=50, log_freq=1000, summary_freq=10, lr_lower_freq=500,
-              grad_clip=10.0,
+              grad_clip=1000.0,
               log_path='./logs/mahendran_vedaldi/vgg16/l1/',
               save_as_mat=True)
 
@@ -23,10 +23,10 @@ params = dict(image_path='./data/selected/images_resized/val13_monkey.bmp', laye
 
 params['lambda_tv'] = 50.0
 
-params['learning_rate'] = 0.004
-params['layer_name'] = 'conv5_2/relu:0'
-params['log_path'] = './logs/mahendran_vedaldi/vgg16/l16/'
-invert_layer(params)
+# params['learning_rate'] = 0.004
+# params['layer_name'] = 'conv5_2/relu:0'
+# params['log_path'] = './logs/mahendran_vedaldi/vgg16/l16/'
+# invert_layer(params)
 
 params['learning_rate'] = 0.004
 params['layer_name'] = 'conv5_3/relu:0'
