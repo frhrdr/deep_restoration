46cced7a4ae27c02b3581b1f7ac611dfd04cf8ea
Fr-d-rik <frederikharder@gmail.com>
2017-07-22 10:16:05 +0200

    prepping conv3/relu full prior training

--- deep_restoration/server_script.py
+++ deep_restoration/server_script.py
@@ -10,11 +10,11 @@ from modules.ica_prior import ICAPrior
 
 ica_prior = ICAPrior(tensor_names='conv3/relu:0',
                      weighting=0.00001, name='ICAPrior',
-                     load_path='../logs/priors/ica_prior/alexnet/5x5_conv3_relu_20kcomp_10kfeats/',
+                     load_path='../logs/priors/ica_prior/alexnet/5x5_conv3_relu_10kcomp_10kfeats/',
                      trainable=False, filter_dims=[5, 5], input_scaling=1.0, n_components=10000, n_channels=384,
                      n_features_white=9599)
 
-ica_prior.train_prior(batch_size=500, num_iterations=50000,
+ica_prior.train_prior(batch_size=250, num_iterations=50000,
                       lr_lower_points=[(0, 1.0e-4), (10000, 1.0e-5), (40000, 3.0e-6), (45000, 1.0e-6)],
                       whiten_mode='pca', data_dir='../data/patches/alexnet/conv3_relu_5x5/',
                       num_data_samples=100000, n_features=9599,
