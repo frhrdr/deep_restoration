4b009cbeeb3b376514a4d59bebf53d44f76826d2
Fr-d-rik <frederikharder@gmail.com>
2017-07-24 16:03:22 +0200

    prepping 3.2k feature conv2 prior generation

--- deep_restoration/ni_tests.py
+++ deep_restoration/ni_tests.py
@@ -4,6 +4,8 @@ from modules.loss_modules import NormedMSELoss, SoftRangeLoss, TotalVariationLos
 from modules.split_module import SplitModule
 from modules.norm_module import NormModule
 from utils.parameter_utils import mv_default_params
+from shutil import copyfile
+
 
 imagenet_mean = (123.68 + 116.779 + 103.939) / 3
 imagenet_mean = [123.68, 116.779, 103.939]
@@ -18,7 +20,7 @@ mse = NormedMSELoss(target='img_rep:0', reconstruction='rec_rep:0', weighting=ms
 #                      trainable=False, filter_dims=[3, 3], input_scaling=1.0, n_components=512, n_channels=96)
 
 ft2_prior = ICAPrior(tensor_names='conv2/relu:0',
-                     weighting=1e-6, name='Conv2Prior',
+                     weighting=1e-8, name='Conv2Prior',
                      load_path='../logs/priors/ica_prior/alexnet/5x5_conv2_relu_2kcomp_1kfeats/ckpt-30000',
                      trainable=False, filter_dims=[5, 5], input_scaling=1.0, n_components=2000, n_channels=256,
                      n_features_white=1000)
@@ -34,11 +36,14 @@ modules = [split, mse, ft2_prior, img_prior]
 
 params = dict(classifier='alexnet',
               modules=modules,
-              log_path='../logs/net_inversion/alexnet/c4_rec/mse4_pimg_pc2_1e-6/',
+              log_path='../logs/net_inversion/alexnet/c4_rec/mse4_pimg_pc2_1e-8/',
               load_path='')
 params.update(mv_default_params())
-params['num_iterations'] = 10000
+params['num_iterations'] = 17000
 params['learning_rate'] = 0.01
+
+copyfile('./ni_tests.py', params['log_path'] + 'script.py')
+
 
 ni = NetInversion(params)
 
@@ -46,5 +51,6 @@ ni = NetInversion(params)
 #                    lr_lower_points=((0, 0.01), (500, 0.001), (1000, 0.0001), (3000, 0.00003), (7000, 0.00001)))
 ni.train_pre_image('../data/selected/images_resized/red-fox.bmp', optim_name='adam',
                    jitter_t=0, jitter_stop_point=0, range_clip=False, scale_pre_img=2.7098e+4,
-                   lr_lower_points=((0, 0.01), (1000, 0.003), (1200, 0.001), (3000, 0.0003),
-                                    (6000, 0.0001), (6000, 0.00003), (9000, 0.00001)))
+                   lr_lower_points=((0, 0.01), (1000, 0.003), (1200, 0.001), (2000, 0.0003),
+                                    (6000, 0.0001), (12000, 0.00003), (15000, 0.00001)))
+
--- deep_restoration/server_script.py
+++ deep_restoration/server_script.py
@@ -1,41 +1,8 @@
-from net_inversion import NetInversion
-from modules.ica_prior import ICAPrior
-from modules.loss_modules import NormedMSELoss, SoftRangeLoss, TotalVariationLoss
-from modules.split_module import SplitModule
-from modules.norm_module import NormModule
-from utils.parameter_utils import mv_default_params
+from utils.temp_utils import make_feat_map_mats, make_reduced_feat_map_mats
 
-imagenet_mean = [123.68, 116.779, 103.939]
-split = SplitModule(name_to_split='conv4/relu:0', img_slice_name='img_rep', rec_slice_name='rec_rep')
-mse = NormedMSELoss(target='img_rep:0', reconstruction='rec_rep:0', weighting=1.)
+make_feat_map_mats(num_patches=100000, map_name='conv2/relu:0', classifier='alexnet', ph=5, pw=5,
+                   save_dir='../data/patches/alexnet/conv2_relu_5x5/', whiten_mode='pca', batch_size=50)
 
-ft3_prior = ICAPrior(tensor_names='conv3/relu:0',
-                     weighting=1.e-7, name='Conv3Prior',
-                     load_path='../logs/priors/ica_prior/alexnet/5x5_conv3_relu_10kcomp_10kfeats/ckpt-25000',
-                     trainable=False, filter_dims=[5, 5], input_scaling=1.0, n_components=10000, n_channels=384,
-                     n_features_white=9599)
-
-img_prior = ICAPrior(tensor_names='pre_img/read:0',
-                     weighting=1.e-3, name='ImgPrior',
-                     load_path='../logs/priors/ica_prior/8by8_512_color/ckpt-10000',
-                     trainable=False, filter_dims=[8, 8], input_scaling=1.0, n_components=512, n_channels=3,
-                     n_features_white=64*3-1)
-
-
-modules = [split, mse, ft3_prior, img_prior]
-
-params = dict(classifier='alexnet',
-              modules=modules,
-              log_path='../logs/net_inversion/alexnet/c4_rec/mse4_pimg_pc3_1e-7/',
-              load_path='')
-params.update(mv_default_params())
-params['num_iterations'] = 10000
-params['learning_rate'] = 0.03
-
-ni = NetInversion(params)
-
-ni.train_pre_image('../data/selected/images_resized/red-fox.bmp', optim_name='adam',
-                   jitter_t=0, jitter_stop_point=0, range_clip=False, scale_pre_img=2.7098e+4,
-                   lr_lower_points=((0, 0.01), (1000, 0.003), (1200, 0.001), (3000, 0.0003),
-                                    (6000, 0.0001), (6000, 0.00003), (9000, 0.00001)),
-                   save_as_mat=True)
+make_reduced_feat_map_mats(100000, load_dir='../data/patches/alexnet/conv2_relu_5x5/',
+                           n_features=6400, n_to_keep=3200,
+                           save_dir='../data/patches/alexnet/conv2_relu_5x5_3200feats/')
