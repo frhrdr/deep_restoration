9908caa38f45b307c700a33a015140b39ee67446
Fr-d-rik <frederikharder@gmail.com>
2017-07-25 11:03:34 +0200

    prepping 3.2k feature conv2 prior training

--- deep_restoration/ni_tests.py
+++ deep_restoration/ni_tests.py
@@ -5,7 +5,7 @@ from modules.split_module import SplitModule
 from modules.norm_module import NormModule
 from utils.parameter_utils import mv_default_params
 from shutil import copyfile
-
+import os
 
 imagenet_mean = (123.68 + 116.779 + 103.939) / 3
 imagenet_mean = [123.68, 116.779, 103.939]
@@ -20,7 +20,7 @@ mse = NormedMSELoss(target='img_rep:0', reconstruction='rec_rep:0', weighting=ms
 #                      trainable=False, filter_dims=[3, 3], input_scaling=1.0, n_components=512, n_channels=96)
 
 ft2_prior = ICAPrior(tensor_names='conv2/relu:0',
-                     weighting=1e-8, name='Conv2Prior',
+                     weighting=1e-9, name='Conv2Prior',
                      load_path='../logs/priors/ica_prior/alexnet/5x5_conv2_relu_2kcomp_1kfeats/ckpt-30000',
                      trainable=False, filter_dims=[5, 5], input_scaling=1.0, n_components=2000, n_channels=256,
                      n_features_white=1000)
@@ -36,12 +36,14 @@ modules = [split, mse, ft2_prior, img_prior]
 
 params = dict(classifier='alexnet',
               modules=modules,
-              log_path='../logs/net_inversion/alexnet/c4_rec/mse4_pimg_pc2_1e-8/',
+              log_path='../logs/net_inversion/alexnet/c4_rec/mse4_pimg_pc2_1e-9/',
               load_path='')
 params.update(mv_default_params())
 params['num_iterations'] = 17000
 params['learning_rate'] = 0.01
 
+if not os.path.exists(params['log_path']):
+    os.makedirs(params['log_path'])
 copyfile('./ni_tests.py', params['log_path'] + 'script.py')
 
 
@@ -52,5 +54,5 @@ ni = NetInversion(params)
 ni.train_pre_image('../data/selected/images_resized/red-fox.bmp', optim_name='adam',
                    jitter_t=0, jitter_stop_point=0, range_clip=False, scale_pre_img=2.7098e+4,
                    lr_lower_points=((0, 0.01), (1000, 0.003), (1200, 0.001), (2000, 0.0003),
-                                    (6000, 0.0001), (12000, 0.00003), (15000, 0.00001)))
+                                    (6000, 0.0001), (10000, 0.00003), (15000, 0.00001)))
 
--- deep_restoration/server_script.py
+++ deep_restoration/server_script.py
@@ -1,8 +1,21 @@
-from utils.temp_utils import make_feat_map_mats, make_reduced_feat_map_mats
+from modules.ica_prior import ICAPrior
 
-make_feat_map_mats(num_patches=100000, map_name='conv2/relu:0', classifier='alexnet', ph=5, pw=5,
-                   save_dir='../data/patches/alexnet/conv2_relu_5x5/', whiten_mode='pca', batch_size=50)
+# make_feat_map_mats(num_patches=100000, map_name='conv2/relu:0', classifier='alexnet', ph=5, pw=5,
+#                    save_dir='../data/patches/alexnet/conv2_relu_5x5/', whiten_mode='pca', batch_size=50)
+#
+# make_reduced_feat_map_mats(100000, load_dir='../data/patches/alexnet/conv2_relu_5x5/',
+#                            n_features=6400, n_to_keep=3200,
+#                            save_dir='../data/patches/alexnet/conv2_relu_5x5_3200feats/')
 
-make_reduced_feat_map_mats(100000, load_dir='../data/patches/alexnet/conv2_relu_5x5/',
-                           n_features=6400, n_to_keep=3200,
-                           save_dir='../data/patches/alexnet/conv2_relu_5x5_3200feats/')
+# kept 97.8% eigv fraction
+
+ica_prior = ICAPrior(tensor_names='conv2/relu:0',
+                     weighting=0.00001, name='ICAPrior',
+                     load_path='../logs/priors/ica_prior/alexnet/5x5_conv2_relu_6400comp_3200feats/',
+                     trainable=False, filter_dims=[5, 5], input_scaling=1.0, n_components=2000, n_channels=256)
+
+ica_prior.train_prior(batch_size=500, num_iterations=30000,
+                      lr_lower_points=[(0, 1.0e-4), (10000, 1.0e-5), (20000, 3.0e-6), (25000, 1.0e-6)],
+                      whiten_mode='pca', data_dir='../data/patches/alexnet/conv2_relu_5x5_3200feats/',
+                      num_data_samples=100000, n_features=1000,
+                      plot_filters=False)
