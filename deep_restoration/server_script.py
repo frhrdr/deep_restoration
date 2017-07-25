b445a2638046ea758166f5d9db227eba171f0e83
Fr-d-rik <frederikharder@gmail.com>
2017-07-25 23:13:29 +0200

    prepping first run with 3.2k prior

--- deep_restoration/server_script.py
+++ deep_restoration/server_script.py
@@ -15,17 +15,17 @@ import os
 
 # kept 97.8% eigv fraction
 
-# ica_prior = ICAPrior(tensor_names='conv2/relu:0',
-#                      weighting=0.00001, name='ICAPrior',
-#                      load_path='../logs/priors/ica_prior/alexnet/5x5_conv2_relu_6400comp_3200feats/',
-#                      trainable=False, filter_dims=[5, 5], input_scaling=1.0, n_components=2000, n_channels=256,
-#                      n_features_white=3200)
-#
-# ica_prior.train_prior(batch_size=200, num_iterations=30000,
-#                       lr_lower_points=[(0, 1.0e-4), (10000, 1.0e-5), (20000, 3.0e-6), (25000, 1.0e-6)],
-#                       whiten_mode='pca', data_dir='../data/patches/alexnet/conv2_relu_5x5_3200feats/',
-#                       num_data_samples=100000, n_features=3200,
-#                       plot_filters=False)
+ica_prior = ICAPrior(tensor_names='conv2/relu:0',
+                     weighting=0.00001, name='ICAPrior',
+                     load_path='../logs/priors/ica_prior/alexnet/5x5_conv2_relu_6400comp_3200feats/',
+                     trainable=False, filter_dims=[5, 5], input_scaling=1.0, n_components=6400, n_channels=256,
+                     n_features_white=3200)
+
+ica_prior.train_prior(batch_size=200, num_iterations=30000,
+                      lr_lower_points=[(0, 1.0e-4), (10000, 1.0e-5), (20000, 3.0e-6), (25000, 1.0e-6)],
+                      whiten_mode='pca', data_dir='../data/patches/alexnet/conv2_relu_5x5_3200feats/',
+                      num_data_samples=100000, n_features=3200,
+                      plot_filters=False)
 
 split = SplitModule(name_to_split='conv4/relu:0', img_slice_name='img_rep', rec_slice_name='rec_rep')
 mse = NormedMSELoss(target='img_rep:0', reconstruction='rec_rep:0', weighting=1.)
