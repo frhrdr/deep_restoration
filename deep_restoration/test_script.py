e2309508619d7cb4e9abda71c8724dd344e76894
Fr-d-rik <frederikharder@gmail.com>
2017-08-06 11:18:29 +0200

    prepping foe conv1lin prior training

--- deep_restoration/test_script.py
+++ deep_restoration/test_script.py
@@ -22,14 +22,14 @@ import numpy as np
 c1_prior = FoEPrior(tensor_names='conv1/lin:0',
                     weighting=1., name='FoEPrior',
                     classifier='alexnet',
-                    filter_dims=[5, 5], input_scaling=1.0, n_components=2400, n_channels=96,
-                    n_features_white=1200)
+                    filter_dims=[5, 5], input_scaling=1.0, n_components=4800, n_channels=96,
+                    n_features_white=2399)
 
-c1_prior.train_prior(batch_size=500, num_iterations=30000, lr=1e-7,
+c1_prior.train_prior(batch_size=250, num_iterations=30000, lr=1e-7,
                     lr_lower_points=((0, 3e-7), (5000, 3e-4), (10000, 3e-4), (25000, 3e-5)),
                     grad_clip=100.0,
                     whiten_mode='pca', num_data_samples=100000,
-                    log_freq=5000, summary_freq=10, print_freq=10, prev_ckpt=0, optimizer_name='adam',
+                    log_freq=5000, summary_freq=10, print_freq=1, prev_ckpt=0, optimizer_name='adam',
                     plot_filters=False, do_clip=True)
 
 
