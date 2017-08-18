2bba49955314dbaaf600a7c9c24cab83f389052c
Fr-d-rik <frederikharder@gmail.com>
2017-08-18 15:50:29 +0200

    prepping ni test

--- deep_restoration/ni_tests.py
+++ deep_restoration/ni_tests.py
@@ -43,7 +43,7 @@ img_prior = ICAPrior(tensor_names='pre_img/read:0',
 #                      filter_dims=[5, 5], input_scaling=1.0, n_components=3000, n_channels=96,
 #                      n_features_white=1800, mean_mode='lc', sdev_mode='gc')
 
-c1l_prior = ChannelICAPrior('conv1_lin:0', 1e-8, 'alexnet', [5, 5], input_scaling=1.0,
+c1l_prior = ChannelICAPrior('conv1_lin:0', 1e-7, 'alexnet', [5, 5], input_scaling=1.0,
                             n_components=150, n_channels=96,
                             n_features_white=24,
                             trainable=False, name='ChannelICAPrior', mean_mode='gc', sdev_mode='gc')
@@ -52,7 +52,7 @@ modules = [split4, mse4, split1, mse1, c1l_prior]
 
 params = dict(classifier='alexnet',
               modules=modules,
-              log_path='../logs/net_inversion/alexnet/c1l_tests_16_08/6_MSE_c4l_CICA_c1l/1e-8/',
+              log_path='../logs/net_inversion/alexnet/c1l_tests_16_08/6_MSE_c4l_CICA_c1l/1e-7/',
               load_path='')
 params.update(mv_default_params())
 params['num_iterations'] = 10000
