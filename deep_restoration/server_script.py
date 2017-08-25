4eec416fef63d8fd76534641aeb54981c27aafef
Fr-d-rik <frederikharder@gmail.com>
2017-08-25 13:42:24 +0200

    making pool1 and c1l gcgc data, training pool1 prior

--- deep_restoration/server_script.py
+++ deep_restoration/server_script.py
@@ -2,24 +2,24 @@ from utils.preprocessing import make_flattened_patch_data
 from modules.ica_prior import ICAPrior
 from modules.foe_prior import FoEPrior
 
-# make_flattened_patch_data(num_patches=100000, ph=5, pw=5, classifier='alexnet', map_name='conv2/lin:0',
-#                           n_channels=256,
-#                           save_dir='../data/patches/alexnet/conv2_lin_5x5_3200feats_mean_lc_sdev_gc/',
-#                           n_feats_white=3200, whiten_mode='pca', batch_size=100,
-#                           mean_mode='lc', sdev_mode='gc')
+make_flattened_patch_data(num_patches=100000, ph=5, pw=5, classifier='alexnet', map_name='pool1:0',
+                          n_channels=96,
+                          save_dir='../data/patches/alexnet/pool1_5x5_2400feats_mean_gc_sdev_gc/',
+                          n_feats_white=2400, whiten_mode='pca', batch_size=100,
+                          mean_mode='gc', sdev_mode='gc')
 #
-# make_flattened_patch_data(num_patches=100000, ph=5, pw=5, classifier='alexnet', map_name='conv2/lin:0',
-#                           n_channels=96,
-#                           save_dir='../data/patches/alexnet/conv1_lin_5x5_1800feats_mean_gc_sdev_gc/',
-#                           n_feats_white=3200, whiten_mode='pca', batch_size=100,
-#                           mean_mode='gc', sdev_mode='gc',
-#                           raw_mat_load_path='../data/patches/alexnet/conv2_lin_5x5_3200feats_mean_lc_sdev_gc/raw_mat.npy')
+make_flattened_patch_data(num_patches=100000, ph=5, pw=5, classifier='alexnet', map_name='conv1/lin:0',
+                          n_channels=96,
+                          save_dir='../data/patches/alexnet/conv1_lin_5x5_1800feats_mean_gc_sdev_gc/',
+                          n_feats_white=1800, whiten_mode='pca', batch_size=100,
+                          mean_mode='gc', sdev_mode='gc',
+                          raw_mat_load_path='')
 
-c1l_prior = FoEPrior(tensor_names='conv1/lin:0',
+c1l_prior = FoEPrior(tensor_names='pool1:0',
                      weighting=1e-12, name='FoEPrior',
                      classifier='alexnet',
-                     filter_dims=[5, 5], input_scaling=1.0, n_components=1000, n_channels=96,
-                     n_features_white=1800, mean_mode='gc', sdev_mode='gc')
+                     filter_dims=[5, 5], input_scaling=1.0, n_components=4800, n_channels=96,
+                     n_features_white=2400, mean_mode='gc', sdev_mode='gc')
 
 c1l_prior.train_prior(batch_size=500, num_iterations=30000, lr=3e-5,
                       lr_lower_points=((0, 1e-0), (7000, 1e-1), (9000, 3e-2),
