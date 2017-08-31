e7d0fb99b52a09a0b81d28b0e3cf4cb5dc7085eb
Fr-d-rik <frederikharder@gmail.com>
2017-08-31 22:21:50 +0200

    training 8x8 c1l foe prior

--- deep_restoration/server_script.py
+++ deep_restoration/server_script.py
@@ -5,15 +5,15 @@ from modules.foe_prior import FoEPrior
 
 
 
-make_flattened_patch_data(num_patches=100000, ph=8, pw=8, classifier='alexnet', map_name='conv1/lin:0',
-                          n_channels=96,
-                          n_feats_white=3000, whiten_mode='pca', batch_size=100,
-                          mean_mode='gc', sdev_mode='gc',
-                          raw_mat_load_path='')
-
-add_flattened_validation_set(num_patches=1000, ph=8, pw=8, classifier='alexnet', map_name='conv1/lin:0',
-                             n_channels=96, n_feats_white=3000, whiten_mode='pca', batch_size=100,
-                             mean_mode='gc', sdev_mode='gc')
+# make_flattened_patch_data(num_patches=100000, ph=8, pw=8, classifier='alexnet', map_name='conv1/lin:0',
+#                           n_channels=96,
+#                           n_feats_white=3000, whiten_mode='pca', batch_size=100,
+#                           mean_mode='gc', sdev_mode='gc',
+#                           raw_mat_load_path='')
+#
+# add_flattened_validation_set(num_patches=1000, ph=8, pw=8, classifier='alexnet', map_name='conv1/lin:0',
+#                              n_channels=96, n_feats_white=3000, whiten_mode='pca', batch_size=100,
+#                              mean_mode='gc', sdev_mode='gc')
 
 prior = FoEPrior(tensor_names='conv1/lin:0',
                  weighting=1e-12, name='FoEPrior',
@@ -22,14 +22,15 @@ prior = FoEPrior(tensor_names='conv1/lin:0',
                  n_features_white=3000, mean_mode='gc', sdev_mode='gc')
 
 prior.train_prior(batch_size=500, num_iterations=24000, lr=3e-5,
-                  lr_lower_points=((0, 1e-0), (6000, 1e-1), (6500, 3e-2),
-                                   (7000, 1e-2), (7500, 3e-3), (8000, 1e-3),
-                                   (85000, 1e-4), (12000, 1e-4), (13000, 1e-5)),
+                  lr_lower_points=(# (0, 1e-0), (6000, 1e-1), (6500, 3e-2),
+                                   # (7000, 1e-2), (7500, 3e-3), (8000, 1e-3),
+                                   # (8500, 1e-4), (12000, 1e-4), (13000, 1e-5)
+                                   (21000, 1e-0), (25000, 1e-4), (30000, 1e-5)),
                   grad_clip=100.0,
                   whiten_mode='pca', num_data_samples=100000,
                   log_freq=1000, summary_freq=10, print_freq=100,
                   test_freq=100, n_val_samples=1000,
-                  prev_ckpt=0,
+                  prev_ckpt=21000,
                   optimizer_name='adam')
 
 prior.plot_filters_all_channels(range(7), prior.load_path + 'filter_vis/')
