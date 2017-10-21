c34b2814ea5678b5ebefe79130be56f0d78b8440
Fr-d-rik <frederikharder@gmail.com>
2017-10-21 21:47:13 +0200

    setting up ensemble attack

--- deep_restoration/utils/mean_filter_benchmark.py
+++ deep_restoration/utils/mean_filter_benchmark.py
@@ -45,8 +45,8 @@ def mean_filter_benchmark(classifier, filter_hw, weightings):
                 print(np.argmax(ref_img), np.argmax(ref_adv))
 
                 fb_model = foolbox.models.TensorFlowModel(img_pl, logit_tsr, bounds=(0, 255))
-                fb_img_pred = fb_model.predictions(image)
-                fb_adv_pred = fb_model.predictions(advex)
+                fb_img_pred = fb_model.predictions(image[0, :, :, :])
+                fb_adv_pred = fb_model.predictions(advex[0, :, :, :])
                 print(np.argmax(fb_img_pred), np.argmax(fb_adv_pred))
                 img_log_list = []
                 adv_log_list = []
