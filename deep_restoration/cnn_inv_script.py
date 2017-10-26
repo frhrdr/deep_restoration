a7e016c1c663d6ef433c79cd639e9655565c5881
Fr-d-rik <frederikharder@gmail.com>
2017-10-26 11:10:32 +0200

    last time dobro

--- deep_restoration/cnn_inv_script.py
+++ deep_restoration/cnn_inv_script.py
@@ -6,7 +6,7 @@ from utils.filehandling import load_image
 import os
 import numpy as np
 
-classifier = 'vgg'
+classifier = 'vgg16'
 start_layer = 3
 rec_layer = 1
 run_stacked_module(classifier, start_layer, rec_layer, use_solotrain=True)
