2cdb4a604655609ab7d9b4ec4a5f1c6b2ed461c9
Fr-d-rik <frederikharder@gmail.com>
2017-10-26 12:05:39 +0200

    last time dobro

--- deep_restoration/mv_script.py
+++ deep_restoration/mv_script.py
@@ -3,7 +3,6 @@ import numpy as np
 from modules.loss_modules import NormedMSELoss, SoftRangeLoss, TotalVariationLoss
 from modules.split_module import SplitModule
 from modules.norm_module import NormModule
-from utils.parameter_defaults import mv_default_params
 
 # imagenet_mean = (123.68 + 116.779 + 103.939) / 3
 imagenet_mean = [123.68, 116.779, 103.939]
@@ -20,23 +19,28 @@ tv_prior = TotalVariationLoss(tensor='pre_img_normed:0', beta=2, weighting=tv_we
 
 modules = [split, norm, mse, sr_prior, tv_prior]
 
-params = dict(classifier='alexnet',
-              modules=modules,
-              log_path='../logs/mahendran_vedaldi/2016/repro/alexnet/conv4_relu/fox/',
-              load_path='')
-params.update(mv_default_params())
-params['num_iterations'] = 12000
-params['learning_rate'] = (0.01 * 80. ** 2 / 6.)
-# params['learning_rate'] = 0.0001
-ni = NetInversion(params)
+classifier = 'alexnet'
 
-pre_img_init = np.reshape(np.load(params['log_path'] + 'mats/rec_500.npy'), [1, 224, 224, 3])
+log_path = '../logs/mahendran_vedaldi/2016/repro/alexnet/conv4_relu/fox/'
+img_path = '../data/selected/images_resized/red-fox.bmp'
+
+load_path = ''
+optimizer = 'momentum'
+print_freq = 50
+log_freq = 500
+summary_freq = 10
+n_iterations = 12000
+lr_factor = 80. ** 2 / 6.
+lr_lower_points = ((0, 1e-2 * lr_factor), (1500, 3e-3 * lr_factor), (3000, 1e-3 * lr_factor))
+
+ni = NetInversion(modules, log_path, classifier='alexnet')
+pre_img_init = np.reshape(np.load(log_path + 'mats/rec_500.npy'), [1, 224, 224, 3])
 # pre_img_init = np.load('../logs/net_inversion/alexnet/c1l_tests_16_08/init_helper.npy')
 # pre_img_init = None
 
 # ni.train_pre_image('../data/selected/images_resized/val13_monkey.bmp', jitter_t=0, optim_name='adam',
 #                    lr_lower_points=((0, 0.01), (500, 0.001), (1000, 0.0001), (3000, 0.00003), (7000, 0.00001)))
-ni.train_pre_featmap('../data/selected/images_resized/red-fox.bmp', optim_name='adam',
+ni.train_pre_featmap(img_path, n_iterations=n_iterations, optim_name='adam',
                      jitter_t=4, jitter_stop_point=3000, range_clip=True, scale_pre_img=1., range_b=80,
-                     lr_lower_points=((1500, (0.003 * 80. ** 2 / 6.)), (3000, (0.001 * 80. ** 2 / 6.))),
+                     lr_lower_points=lr_lower_points,
                      pre_featmap_init=pre_img_init, ckpt_offset=0, save_as_plot=True)
