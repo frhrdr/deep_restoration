895c30b3d13c4d3291bcf0ab7a77ca7065cb4e0c
Fr-d-rik <frederikharder@gmail.com>
2017-10-29 20:24:52 +0100

    last time dobro

--- deep_restoration/utils/default_priors.py
+++ deep_restoration/utils/default_priors.py
@@ -2,20 +2,20 @@ from modules.foe_dropout_prior import FoEDropoutPrior
 from modules.foe_full_prior import FoEFullPrior
 
 
-def get_default_prior(mode, custom_weighting=None):
+def get_default_prior(mode, custom_weighting=None, custom_target=None):
     assert mode in ('full512', 'dropout1024', 'dropout_nodrop_train1024', 'dropout_nodrop_train512', 'fullc1l6000',
                     'slimc5l5000', 'full512logistic')
     if mode == 'full512':
-        p = FoEFullPrior('pre_featmap:0', 1e-5, 'alexnet', [8, 8], 1.0, n_components=512, n_channels=3,
+        p = FoEFullPrior('rgb_scaled:0', 1e-5, 'alexnet', [8, 8], 1.0, n_components=512, n_channels=3,
                          n_features_white=8 ** 2 * 3 - 1, dist='student', mean_mode='gc', sdev_mode='gc',
                          whiten_mode='pca',
                          name=None, load_name='FoEPrior', dir_name=None, load_tensor_names='image')
     elif mode == 'full512logistic':
-        p = FoEFullPrior('pre_featmap:0', 1e-5, 'alexnet', [8, 8], 1.0, n_components=512, n_channels=3,
+        p = FoEFullPrior('rgb_scaled:0', 1e-5, 'alexnet', [8, 8], 1.0, n_components=512, n_channels=3,
                          n_features_white=8 ** 2 * 3 - 1, dist='logistic', mean_mode='gc', sdev_mode='gc',
                          whiten_mode='pca', load_tensor_names='image')
     elif mode == 'dropout1024':
-        p = FoEDropoutPrior('pre_featmap:0', 1e-5, 'alexnet', [8, 8], 1.0, n_components=1024, n_channels=3,
+        p = FoEDropoutPrior('rgb_scaled:0', 1e-5, 'alexnet', [8, 8], 1.0, n_components=1024, n_channels=3,
                             n_features_white=8 ** 2 * 3 - 1, dist='student', mean_mode='gc', sdev_mode='gc',
                             whiten_mode='pca', load_tensor_names='image',
                             activate_dropout=True, make_switch=False, dropout_prob=0.5)
@@ -48,4 +48,6 @@ def get_default_prior(mode, custom_weighting=None):
                             activate_dropout=True, make_switch=False, dropout_prob=0.5)
     if custom_weighting is not None:
         p.weighting = custom_weighting
+    if custom_target is not None:
+        p.in_tensor_names = custom_target
     return p
