0933002296b35aecace8c2e231412373adc7db3d
Fr-d-rik <frederikharder@gmail.com>
2017-11-02 18:40:36 +0100

    featmap inv

--- deep_restoration/foe_inv_featmap_script.py
+++ deep_restoration/foe_inv_featmap_script.py
@@ -3,11 +3,11 @@ from foe_inv_funcs import featmap_inv
 match_layer = 2
 target_layer = 1
 jitter_t = 0
-weighting = '1e-3'
+weighting = '1e-5'
 make_mse = False
 restart_adam = False
 image_name = 'val153'
-pre_image = False
+pre_image = True
 do_plot = True
 
 
@@ -24,3 +24,13 @@ pre_image = True
 featmap_inv(match_layer, target_layer, image_name, prior_id, prior_weighting=weighting, make_mse=make_mse,
             restart_adam=restart_adam, pre_image=pre_image, do_plot=do_plot,
             jitter_t=0, jitter_stop_point=3200, lr=1., bound_plots=True)
+
+
+# pre_image = True
+# prior_id = 'full512'
+# weighting = '1e-4'
+# custom_target = 'pre_featmap/read:0'
+# match_layer = 6
+# featmap_inv(match_layer, target_layer, image_name, prior_id, prior_weighting=weighting, make_mse=make_mse,
+#             restart_adam=restart_adam, pre_image=pre_image, do_plot=do_plot,
+#             jitter_t=0, jitter_stop_point=3200, lr=1., bound_plots=True, custom_target=custom_target)
