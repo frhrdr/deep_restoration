9bf50aca510d8d3b6d9527a614cbd9bb2fd0f535
Fr-d-rik <frederikharder@gmail.com>
2017-11-03 11:14:55 +0100

    featmap inv

--- deep_restoration/utils/mean_filter_benchmark.py
+++ deep_restoration/utils/mean_filter_benchmark.py
@@ -35,7 +35,7 @@ def adaptive_mean_filter_exp():
 def fgsm_mean_filter_exp():
     weighings = (0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.)
     mean_filter_benchmark(classifier='alexnet', filter_hw=2, weightings=weighings,
-                          advex_subdir='alexnet_val_2k_top1_correct/gradient_oblivious/')
+                          advex_subdir='alexnet_val_2k_top1_correct/gradientsign_oblivious/')
     mean_log_statistics(plot=False)
 
 
