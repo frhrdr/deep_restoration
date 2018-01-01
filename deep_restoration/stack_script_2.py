0baf5111bb88e2e1af0b2a9bd1ea285be104dbed
Fr-d-rik <frederikharder@gmail.com>
2018-01-01 16:59:15 +0100

    final touches, vgg reconstructions

--- deep_restoration/stack_script_2.py
+++ deep_restoration/stack_script_2.py
@@ -4,9 +4,9 @@ from foe_inv_funcs import run_image_opt_inversions
 # db_lin_to_img_gen('alexnet', use_solotrain=True)
 # db_lin_to_img_gen('alexnet', use_solotrain=False)
 
-run_image_opt_inversions('vgg16', 'full512', layer_select='pool5', lr=1., mse_iterations=10000,
+run_image_opt_inversions('alexnet', 'full512', layer_select='lin', lr=0.3, mse_iterations=10000,
                          jitterations=6900, opt_iterations=15000,
-                         select_img=9)
+                         select_img=None)
 
 # run_image_opt_inversions('vgg16', 'full512', layer_select='fc7/lin', lr=1., mse_iterations=5000, opt_iterations=10000,
 #                          select_img=6)
