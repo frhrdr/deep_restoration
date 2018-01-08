ac116b0ca3d10475924765933b7a469c055a18a1
Fr-d-rik <frederikharder@gmail.com>
2018-01-08 12:57:12 +0100

    beginning clean-up, preparing presentation

--- deep_restoration/mode_exploration_script.py
+++ deep_restoration/mode_exploration_script.py
@@ -2,10 +2,6 @@ from foe_inv_funcs import run_image_opt_inversions
 
 
 def run_fc(select_img):
-    run_image_opt_inversions('vgg16', 'full512', layer_select='fc6/lin', lr=.3, mse_iterations=10000, opt_iterations=15000,
-                             jitterations=6900,
-                             select_img=select_img)
-
     run_image_opt_inversions('vgg16', 'full512', layer_select='fc7/lin', lr=.3, mse_iterations=10000, opt_iterations=15000,
                              jitterations=6900,
                              select_img=select_img)
